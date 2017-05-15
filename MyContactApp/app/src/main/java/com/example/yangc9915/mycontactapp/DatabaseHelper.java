43d4fec8f46130d4edeb1ae70f03d09c269015db
cyyangg <cly9915@gmail.com>
2017-05-15 09:25:09 -0700

    Updated DatabaseHelper and MainActivity to create InsertData method and toast message, added Age and Phone Number fields
    Insert Data method
    toast message indicating success or fail
    age and phone number fields

--- MyContactApp/app/src/main/java/com/example/yangc9915/mycontactapp/DatabaseHelper.java
+++ MyContactApp/app/src/main/java/com/example/yangc9915/mycontactapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.yangc9915.mycontactapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -29,5 +30,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
 
+    }
+
+    public boolean insertData(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1) return false;
+        else return true;
     }
 }
--- MyContactApp/app/src/main/java/com/example/yangc9915/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/yangc9915/mycontactapp/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.yangc9915.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAge;
+    EditText editPhoneNum;
     Button btnAddData;
 
 
@@ -18,5 +23,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+        editAge = (EditText) findViewById(R.id.editText_age);
+        editPhoneNum = (EditText) findViewById(R.id.editText_phoneNum);
+    }
+
+    public void addData(View v) {
+        boolean isInsertedName = myDb.insertData(editName.getText().toString());
+        boolean isInsertedAge = myDb.insertData(editAge.getText().toString());
+        boolean isInsertedPhoneNum = myDb.insertData(editPhoneNum.getText().toString());
+
+        if(isInsertedName == true && isInsertedAge == true && isInsertedPhoneNum == true){
+            Log.d("MyContact", "Data insertion successful");
+            //create toast message to user indicating data inserted correctly
+            Toast.makeText(getApplicationContext(), "Data insertion successful", Toast.LENGTH_SHORT).show();
+        }
+
+        else{
+            Log.d("MyContact", "Data insertion NOT successful");
+            //create toast message to user indicating data inserted incorrectly
+            Toast.makeText(getApplicationContext(), "Data insertion NOT successful", Toast.LENGTH_SHORT).show();
+        }
     }
 }
